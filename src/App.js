f7b223302ad0c1efc23c23c2cc5834ac906c49bb
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-07-23 23:57:00 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => {
         if (response) {
-          fetch(" https://guarded-wave-42693.herokuapp.com/image", {
+          fetch("https://guarded-wave-42693.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
